275349879648fc3ad8d232aa996e8e7d509f150d
Alessandro Jeanteur <aless.jeant@gmail.com>
2018-07-27 11:09:24 +0200

    smallfix

--- app.py
+++ app.py
@@ -19,13 +19,13 @@ def redirect_first_page(chapter):
 @app.route('/<chapter>/<page>')
 def render_page(chapter=900, page=1):
     url = 'https://www.mangapanda.com/one-piece/' + str(chapter) + ('/' + str(page) if int(page)>1 else '')
-    html = requests.get(url).text
-    server_url = request.host_url
-    soup = BeautifulSoup(html, 'html.parser')
     try:
-        img_url =soup.find('img', {'id':'img'}).get('src')
+        html = requests.get(url).text
     except:
         return redirect(url_for(render_page(chapter+1)))
+    server_url = request.host_url
+    soup = BeautifulSoup(html, 'html.parser')
+    img_url =soup.find('img', {'id':'img'}).get('src')
     return '<div><a href="{prev}">prev</a> <a href="{next}">next</a></div><a href={next}><img src={cur} style="width=100%;height=auto;"/></a>'.format(prev=server_url+str(chapter)+'/'+str(int(page)-1), next=server_url+str(chapter)+'/'+str(int(page)+1), cur=img_url)
 
 if __name__ == '__main__':

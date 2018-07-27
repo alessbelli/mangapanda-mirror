802d264c3c48b4faf5d6e6fc6a2a6a5d59b504fb
Alessandro Jeanteur <aless.jeant@gmail.com>
2018-07-27 11:24:04 +0200

    fix

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ def render_page(chapter=900, page=1):
     url = 'https://www.mangapanda.com/one-piece/' + str(chapter) + ('/' + str(page) if int(page)>1 else '')
     try:
         html = requests.get(url).text
+        server_url = request.host_url
+        soup = BeautifulSoup(html, 'html.parser')
+        img_url =soup.find('img', {'id':'img'}).get('src')
     except:
-        return redirect(url_for(render_page(chapter+1)))
-    server_url = request.host_url
-    soup = BeautifulSoup(html, 'html.parser')
-    img_url =soup.find('img', {'id':'img'}).get('src')
+        return redirect(url_for(render_page(chapter + 1)))
     return '<div><a href="{prev}">prev</a> <a href="{next}">next</a></div><a href={next}><img src={cur} style="width=100%;height=auto;"/></a>'.format(prev=server_url+str(chapter)+'/'+str(int(page)-1), next=server_url+str(chapter)+'/'+str(int(page)+1), cur=img_url)
 
 if __name__ == '__main__':
